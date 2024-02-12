70020e47b2dfaa33de9e307861b93170819f801b
MarlonChi <marlonchiodelli@hotmail.com>
2024-02-12 16:27:07 -0300

    feat: post

---
+++ src/components/Post.jsx
@@ -0,0 +1,46 @@
+import * as styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/MarlonChi.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Marlon Chiodelli</strong>
+            <span>WebDeveloper</span>
+          </div>
+        </div>
+        <time title="12 de Fevereiro as 15:40" dateTime="2024-02-21 15:40:00">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p> Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href="">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#rocketseat</a>{" "}
+          <a href="">#nlw</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Comentar</button>
+        </footer>
+      </form>
+    </article>
+  );
+}
