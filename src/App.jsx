3153b201ac953cc57f43e4f8cec83b42849539ed
MarlonChi <marlonchiodelli@hotmail.com>
2024-02-12 15:24:23 -0300

    feat: sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>Posts</main>
+      </div>
     </div>
   );
 }
