6ea5e78e1785e5733a92906a501aff0efedcc180
MarlonChi <marlonchiodelli@hotmail.com>
2024-02-14 11:54:39 -0300

    feat: like function

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ThumbsUp, Trash } from "phosphor-react";
 
 import { Avatar } from "./Avatar";
@@ -5,8 +6,14 @@ import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
 
 export function Comment({ comment, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(comment);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -35,9 +42,9 @@ export function Comment({ comment, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
